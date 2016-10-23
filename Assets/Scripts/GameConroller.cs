fd5780503635f0612b919206fa8fdaf444e5e032
Gaming <fearoo90@gmail.com>
2016-10-23 05:41:04 +0300

    Added Restart Level

---
+++ Assets/Scripts/GameConroller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameConroller : MonoBehaviour {
+
+
+	void Update ()
+	{
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);	
+		}
+
+	}
+}
