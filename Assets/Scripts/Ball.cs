1f9dbc1bf57c2c276c7df6c045c2b067479664f6
Gaming <fearoo90@gmail.com>
2017-01-12 01:51:15 +0300

    Refactored Ball and Paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,10 +9,11 @@ public class Ball : MonoBehaviour
 {
 
 	public float velocity;
-	public bool stickToPaddle;
+	public bool isLocked;		//Shows if ball is currently locked to the paddle
+//	public bool stickToPaddle;
 
-	private Paddle paddle;
-	private Vector3 ballDefaultPosition;
+//	private Paddle paddle;
+//	private Vector3 ballDefaultPosition;
 
 	private Rigidbody2D rg;
 	private Collider2D collider2d;
@@ -21,12 +22,8 @@ public class Ball : MonoBehaviour
 	private SoundController soundController;
 
 	private float minPlaybackOffset	= 0.1f;
-	private Vector3 stuckPosition;
-	private bool isStuckToPaddle = false;
-//	private float paddleInteractiveHeight = 0.02f;   TODO Delete after refactoring
 	//Offset between the starting time of the previous and the current audio playback.
 	private static float previousAudioStartTime;
-	//The time the previous explosion sound started the playback
 
 
 	void Awake ()
@@ -37,64 +34,59 @@ public class Ball : MonoBehaviour
 		collider2d = GetComponent<Collider2D> ();
 	}
 
-	// Use this for initialization
-	void Start ()
-	{
-
-		paddle = GameObject.FindObjectOfType<Paddle> ();
-		if (paddle)
-			ballDefaultPosition = transform.position - paddle.transform.position;
-		else
-			Debug.LogWarning("Ball init failed: No paddle was found");
-	}
-
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		
-		//#TODO Take everything except of ChangeVelocity into Paddle
-		if (!animator.GetBool ("isLaunched")) {
-			LockToPaddle();
-			if (Input.GetMouseButtonDown (0)) {
-				Launch ();
-			}
-		} else {
+		if (animator.GetBool ("isLaunched")) {
 			ChangeVelocity (velocity);
-//			RandomTweak();
-		}
-
-
+		} 
 	}
+
 
 	void OnCollisionEnter2D (Collision2D collision)
 	{
 		PlayHitSound ();
+	}
 
-		if (collision.gameObject.tag == "Paddle") {
-		// #TODO take this module into paddle
-			if (stickToPaddle) {
-				Stick ();
-			} else {
-				ChangeDirectionRelativelyTo (collision.gameObject, 0.02f);
-			}
+
+
+	//#Call Launch from Paddle Launch.
+	public void Launch (Vector2 direction)
+	{
+		collider2d.enabled = true;
+		rg.velocity = new Vector2 (0f, velocity);
+		ChangeDirection (direction);
+		animator.SetBool ("isLaunched", true);
+		isLocked = false;
+	}
+
+
+	public void Stop ()
+	{
+		animator.SetBool ("isLaunched", false);		
+		collider2d.enabled = false;
+		ChangeVelocity (0f);
+		isLocked = true;
+	}
+
+
+	public void Follow (GameObject target, Vector3 relativePosition) {
+		transform.position = target.transform.position + relativePosition;
+	}
+
+
+	public void ChangeDirection (Vector2 direction)
+	{
+		rg.velocity = direction.normalized * rg.velocity.magnitude;
+	}
+
+
+
+	void ChangeVelocity (float targetVelocity)
+	{
+		if (rg.velocity.magnitude != targetVelocity) {
+			rg.velocity = rg.velocity.normalized * targetVelocity;
 		}
-	}
-
-
-
-	public void Destroy ()
-	{
-		Stop ();
-		animator.SetTrigger ("Explosion");
-	}
-
-	//Changes direction of the ball when it hits the paddle depending on the point of hit of the paddle
-	public void ChangeDirectionRelativelyTo (GameObject targetPaddle, float paddleInteractiveHeight)
-	{
-	// #TODO take except of the IF body into the paddle
-		Vector2 direction = transform.position - targetPaddle.transform.position;
-		if (direction.y > paddleInteractiveHeight)	   //Preventing the ball from stucking in boring loop
-			ChangeDirection (direction);
 	}
 
 
@@ -106,62 +98,18 @@ public class Ball : MonoBehaviour
 			soundController.PlaySound ();
 		}
 	}
-	
-	//#Call Launch from Paddle Launch.
-	//#TODO This method should be called only after ChangeDirectionRelativelyTo in Paddle class. 
-	public void Launch ()
-	{
-		collider2d.enabled = true;
-		isStuckToPaddle = false;
-		rg.velocity = new Vector2 (0f, velocity);
-		//#TODO Call ChangeDirecionRelativelyTo() from Paddle.
-		ChangeDirectionRelativelyTo (paddle.gameObject, 0.02f);
-		animator.SetBool ("isLaunched", true);
-	}
-
-	void Stop ()
-	{
-		animator.SetBool ("isLaunched", false);		
-		collider2d.enabled = false;
-		ChangeVelocity (0f);
-	}
-	//#TODO Take this method into Ball
-	void Stick ()
-	{
-		stuckPosition = transform.position - paddle.transform.position;
-		if (stuckPosition.y > 0.02f) {							//#TODO change 0.02f to PaddleInteractiveHeight after refactoring
-			isStuckToPaddle = true;
-			Stop ();
-		}
-	}
-
-	void LockToPaddle ()
-	{
-		if (isStuckToPaddle) {
-			transform.position = paddle.transform.position + stuckPosition;
-		} else {
-			transform.position = paddle.transform.position + ballDefaultPosition;
-		}
-	}
-
-	void ChangeVelocity (float targetVelocity)
-	{
-		if (rg.velocity.magnitude != targetVelocity) {
-			rg.velocity = rg.velocity.normalized * targetVelocity;
-		}
-	}
-
-	void ChangeDirection (Vector2 direction)
-	{
-		rg.velocity = direction.normalized * rg.velocity.magnitude;
-	}
 
 	//randomly changes velocity of the ball a bit to prevent stucking / inifinite loop
-	void RandomTweak ()
-	{
-		Vector2 tweakVelocity = new Vector2 (Random.Range (0, 0.1f), Random.Range (0, 0.1f));
-		rg.velocity += tweakVelocity;
-	}
+//	void RandomTweak ()
+//	{
+//		Vector2 tweakVelocity = new Vector2 (Random.Range (0, 0.1f), Random.Range (0, 0.1f));
+//		rg.velocity += tweakVelocity;
+//	}
 
+	public void Destroy ()
+	{
+		Stop ();
+		animator.SetTrigger ("Explosion");
+	}
 
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,20 +1,112 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
 public class Paddle : MonoBehaviour {
 
 
+	public float VerticalOffset;	//Vertical offset above which there will be no movement in pixels
+	public float accelerationForce;
+	public bool controlledByAI;
+	public bool stickToPaddle;
+	public Ball playerBall;
+
+
+	Camera mainCamera;
+	Rigidbody2D rigid;
+	float maximumMouseHeight;
+
 	private Animator animator;
 
+//	private Vector3 ballDefaultPosition; //new
+	private Vector3 lockedPosition; 
+	private float InteractiveHeight = 0.02f;
 
 	// Use this for initialization
 	void Start ()
 	{
 		animator = GetComponent<Animator> ();
+		rigid = GetComponent<Rigidbody2D>();
+		mainCamera = Camera.main;
+
+
+		//Calculating the line above which LaunchBall method will be invoked and below which MovePaddle will be invoked.
+		Vector3 position = mainCamera.WorldToScreenPoint(transform.position);
+		maximumMouseHeight =  position.y + VerticalOffset;
+
+		lockedPosition = CalculateRelativeDirection(playerBall.gameObject);		//default position of the ball
+	}
+
+	void FixedUpdate ()
+	{
+		//Move paddle depending on who is controlling it (AI/Player)
+		if (controlledByAI)
+			MoveToThePoint (playerBall.transform.position, 0.5f);
+		else
+			MoveWithTouch ();
+
+		// Lock the ball to the paddle
+		if (playerBall.isLocked)
+			playerBall.Follow (gameObject, lockedPosition);
+
+		if (playerBall.isLocked && Input.GetMouseButtonUp (0))
+			Launch (playerBall);
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ball") {
+			Ball collidedBall = collision.gameObject.GetComponent<Ball>();
+			Vector2 direction = CalculateRelativeDirection(collision.gameObject);
+			if (stickToPaddle)
+				Lock (collidedBall);
+			else if (direction.y > InteractiveHeight) //Preventing the ball from stucking in boring loop
+				collidedBall.ChangeDirection (direction);
+		}
+	}
+
+	void MoveWithTouch ()
+	{
+		if (Input.GetMouseButton (0) && Input.mousePosition.y <= maximumMouseHeight) 
+			MoveToThePoint (mainCamera.ScreenToWorldPoint(Input.mousePosition), 0.1f);
 	}
 
 
+	void MoveToThePoint (Vector3 point, float sensitivity)
+	{
+		// Find direction from  to ball
+		Vector3 direction = point - transform.position;
+
+		// Move  with force if ball is further than 0.5 metre
+		if (Mathf.Abs (direction.x) > sensitivity) {
+			direction = direction.x * Vector3.right;
+			rigid.AddForce (direction.normalized * accelerationForce, ForceMode2D.Impulse);
+		} else {
+			rigid.velocity = Vector2.zero;
+		}
+	}
+
+	void Launch (Ball ball)
+	{
+//		if (Input.mousePosition.y > maximumMouseHeight && Input.GetMouseButtonUp(0)) 
+			// Calculating bounce 
+			Vector2 direction = CalculateRelativeDirection(ball.gameObject);
+			ball.Launch (direction);
+	}
+
+	void Lock (Ball ball)
+	{
+		Vector2 relativePosition = CalculateRelativeDirection (ball.gameObject);
+		if (relativePosition.y > InteractiveHeight)	
+			lockedPosition = relativePosition;
+			ball.Stop ();
+	}
+
+
+	Vector2 CalculateRelativeDirection (GameObject target) {
+		return target.transform.position - transform.position;
+	}
 
 	public void Destroy ()
 	{	
